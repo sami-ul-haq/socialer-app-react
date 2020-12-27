f02cc0dedad5308cb2b58e8dbbb8803a07100601
sami-ul-haq <samiulhaq1899@gmail.com>
2020-12-27 23:04:46 +0500

    fix1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Home from "./pages/home/Home";
 import { UserContextProvider } from "./context/userContext";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
